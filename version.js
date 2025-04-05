ac4820ebecd6ca29ff8c30919a70f880ad491a9b
jjarryan <yuje0518@gmail.com>
2025-04-05 22:22:37 +0900

    Add files via upload

---
+++ version.js
@@ -0,0 +1,12 @@
+﻿{
+  "files": [
+    {
+      "path": "Data/Item/Etc/0443.img",
+      "url": "https://jjarryan.github.io/maple-patch/Item/Etc/0443.img"
+    },
+    {
+      "path": "Data/String/Etc.img",
+      "url": "https://jjarryan.github.io/maple-patch/String/Etc.img"
+    }
+  ]
+}
